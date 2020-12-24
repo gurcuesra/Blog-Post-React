2d34c5881ad1db6644c1b6b8aa926ea74e0f46a0
gurcuesra <gurcuesra@gmail.com>
2020-12-23 18:30:13 -0800

    Move components in to the App.js

--- src/index.js
+++ src/index.js
@@ -4,40 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const myBlogPost = {
-  title: 'My first blog',
-  body: 'lorem ipsum dolor sit amet...',
-  published: true
-}
 
-let classProperties;
-if(myBlogPost.published) {
-  classProperties='green';
-} else {
-  classProperties="red";
-}
-
-function publishThePost(e) {
-  if(e.target.innerText === "Publish"){
-    e.target.innerText = "Unpublish";
-    document.getElementById('title').style.color = "green";
-  } else {
-    e.target.innerText = "Publish";
-    document.getElementById('title').style.color = "red";
-  }
-  
-}
-
-const blogPost = (
-  <div>
-    <h1 id="title" className={classProperties}>{myBlogPost.title}</h1>
-    <p>{myBlogPost.body}</p>
-    <button onClick={publishThePost}>Unpublish</button>
-    <img src="" alt=""/>
-  </div>
-)
 ReactDOM.render(
-  blogPost,
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
